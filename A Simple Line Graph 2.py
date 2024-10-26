be83d35c20deff947be81df2c85c0825af1a584f
Munashe Samapundo <samapundomunashe@gmail.com>
2024-10-26 20:36:49 +0200

    Update A Simple Line Graph 2.py

--- A Simple Line Graph 2.py
+++ A Simple Line Graph 2.py
@@ -1,8 +1,9 @@
 import matplotlib.pyplot as plt
+input_values = [1,2,3,4,5]
 squares = [1,4,9,16,25]
-plt.plot(squares, linewidth = 5)
+plt.plot(input_values,squares, linewidth=5)
 plt.ylabel('Values of Squares', fontsize=14)
 plt.xlabel('Values', fontsize=14)
-plt.title('A Simple Line Graph 2', fontsize=24)
-plt.tick_params(axis='both', labelsize=14)
+plt.title('A Simple Line Graph', fontsize=14)
+plt.tick_params(axis='both', labelsize=15)
 plt.show()
